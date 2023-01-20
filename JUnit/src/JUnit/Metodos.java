05df69f5af9989d46622f12f554d06d693aa2b8f
in1dw3 <in1dw3@HZ352015.elorrieta.int>
2023-01-20 16:41:05 +0100

    Cambio tipos de dato y bucle suma

---
+++ JUnit/src/JUnit/Metodos.java
@@ -0,0 +1,66 @@
+package JUnit;
+
+public class Metodos {
+	public boolean esPar(int a) {
+		boolean par = true;
+		if (a % 2 == 0) {
+			par = true;
+		} else {
+			par = false;
+		}
+		return par;
+	}
+
+	public int esMayor(int a, int b) {
+		int mayor = 0;
+		if (a > b) {
+			mayor = a;
+		} else {
+			mayor = b;
+		}
+		return mayor;
+	}
+
+	public boolean esPrimo(int a) {
+		boolean primo = true;
+		if (a == 0 || a == 1) {
+			primo = false;
+		} else {
+			for (int i = 2; i <= (a / 2); i++) {
+				if (a % i == 0) {
+					primo = false;
+				}
+			}
+		}
+		return primo;
+	}
+
+	public int suma(int a, int b) {
+		int resultado = a + b;
+		return resultado;
+
+	}
+
+	public int resta(int a, int b) {
+		int resultado = a - b;
+		return resultado;
+
+	}
+
+	public int multi(int a, int b) {
+		int resultado = a * b;
+		return resultado;
+
+	}
+
+	public double divi(double a, double b) {
+		double resultado = 0;
+		if (b == 0) {
+			return -1;
+		} else {
+			resultado = a / b;
+			return resultado;
+		}
+
+	}
+}
